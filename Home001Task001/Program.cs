2b8ab94742d87491f4388b9d0fb866d0cf330d8d
Ekaterina <katya-kosyuga@mail.ru>
2022-11-20 20:01:12 +0500

    большее и меньшее число из двух

---
+++ Home001Task001/Program.cs
@@ -0,0 +1,28 @@
+﻿// вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.WriteLine("Введи первое число");
+
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введи второе число");
+
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (A > B)
+{    
+    Console.Write("Первое число ");
+    Console.Write(A);
+    Console.Write(" больше второго числа ");
+    Console.Write(B);
+}
+    else 
+    {
+    Console.Write("Второе число ");
+    Console.Write(B);
+    Console.Write(" больше первого числа ");
+    Console.Write(A);;  
+    }
+
+
+
+
