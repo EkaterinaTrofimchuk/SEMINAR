47c098e1924bb4e75ae52b9b23f8d60d4ffaf0df
Ekaterina <katya-kosyuga@mail.ru>
2022-11-26 19:14:29 +0500

    возведение в степень двух чисел

---
+++ Home004Task001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введи первое число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе число");
+int B = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int St = 1;
+
+while (i<B)
+{
+St = St*A;
+i++;
+}
+
+Console.Write($" результат возведения в степень равен {St}");
+
+
