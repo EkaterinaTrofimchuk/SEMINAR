98c067b5d58b79986aa45396ac97d4d7b564c390
Ekaterina <katya-kosyuga@mail.ru>
2022-11-26 20:04:14 +0500

    сумма цифр в числе

---
+++ Home004Task002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе
+
+int number = new Random().Next();
+Console.WriteLine(number);
+
+int sum = 0;
+
+while (number>0)
+{
+sum = sum + (number % 10);
+number = number / 10;
+}
+
+Console.WriteLine(sum);
