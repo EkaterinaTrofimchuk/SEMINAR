2684866510c4ed4af4de2cb8458415126954d789
Ekaterina <katya-kosyuga@mail.ru>
2022-12-04 22:50:01 +0500

    рекурсия, вывод чисел от 1 до N

---
+++ Home009Task001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все 
+//натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+string NumbersRec(int N, int b)
+{
+    if (N<=b) return NumbersRec(N+1, b) + $"{N}  ";
+    else return string.Empty;
+}
+
+Console.WriteLine(NumbersRec(1,8));
