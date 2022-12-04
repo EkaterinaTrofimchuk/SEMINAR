5066b6d9cf626d5028829f7eec1ee75b7bfeae9a
Ekaterina <katya-kosyuga@mail.ru>
2022-12-04 21:15:26 +0500

    трехмерный массив

---
+++ Home008Task003/Program.cs
@@ -0,0 +1,14 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел
+
+int[,,] arr = new int [3,4,5];
+arr [1,2,3] = 3;
+for (int row = 0; row < arr.GetLength(0); row++)
+{
+    for (int columns = 0; columns < arr.GetLength(1); columns++)
+    {
+        for (int side = 0; side < arr.GetLength(2); side++)
+        {
+            Console.WriteLine($"{arr[row, columns, side]}({row},{columns},{side})");
+        }
+    }
+}
