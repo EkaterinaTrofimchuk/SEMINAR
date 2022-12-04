cb26d7f2a499829ed525795307af7ccaf8e5cf01
Ekaterina <katya-kosyuga@mail.ru>
2022-12-04 23:23:51 +0500

    рекурсия, сумма чисел от м  до N

---
+++ Home009Task002/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumRec (int m, int n)
+{
+    int sum = 0;
+    int innerRec(int innM, int innN)
+    {
+        if (innM <= innN) {
+            sum += innM;
+            return innerRec(innM + 1, innN);
+        } else {
+            return sum;
+        }
+    }
+
+    return innerRec(m, n);
+}
+
+Console.WriteLine(SumRec(4,10));
